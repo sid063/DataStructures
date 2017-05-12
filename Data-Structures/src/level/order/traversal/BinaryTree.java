4b5c5d72e8bd0f20924ebd10f570c7c2c5e9c0fc
Siddharth Gupta <siddharth063@yahoo.com>
2017-05-12 16:03:45 +0530

    Update BinaryTree.java

--- Data-Structures/src/level/order/traversal/BinaryTree.java
+++ Data-Structures/src/level/order/traversal/BinaryTree.java
@@ -20,9 +20,19 @@ public class BinaryTree {
 	private void printLevelOrder(Node node) {
 		int height = height(node);
 		for (int i = 1; i <= height; i++) {
-			System.out.println();
-			// To print levels line by line.
+			System.out.println(); // To print levels line by line.
 			printGivenLevel(node, i);
+		}
+	}
+	
+	private static void printGivenLevel(Node node, int level) {
+		if (node == null)
+			return;
+		if (level == 1) {
+			System.out.print(node.key + " ");
+		} else if (level > 1) {
+			printGivenLevel(node.left, level - 1);
+			printGivenLevel(node.right, level - 1);
 		}
 	}
 
@@ -41,32 +51,29 @@ public class BinaryTree {
 
 	}
 
-	private static void printGivenLevel(Node node, int level) {
-		if (node == null)
-			return;
-		if (level == 1) {
-			System.out.print(node.key + " ");
-		} else if (level > 1) {
-			printGivenLevel(node.left, level - 1);
-			printGivenLevel(node.right, level - 1);
-		}
-	}
-
 	public static void main(String[] args) {
+		
+		/*
+		 * 	    1
+		 *  	  /    \
+		 *      2       3
+		 *    /	  \   /	  \
+		 *   4    5   7    8
+		 *      /   \
+		 *     10   11
+		 *    /
+		 *   12
+		 */
 		BinaryTree tree = new BinaryTree();
 		tree.root = new Node(1);
 		tree.root.left = new Node(2);
 		tree.root.right = new Node(3);
-
 		tree.root.left.left = new Node(4);
 		tree.root.left.right = new Node(5);
-
 		tree.root.right.left = new Node(7);
 		tree.root.right.right = new Node(8);
-
 		tree.root.left.right.left = new Node(10);
 		tree.root.left.right.right = new Node(11);
-
 		tree.root.left.right.left.left = new Node(12);
 
 		System.out.print("Level order traversal of binary tree is ");
