ac00d64c56009dedfb1b1f30f512260174e8f907
Siddharth Gupta <siddharth063@yahoo.com>
2017-05-13 18:42:43 +0530

    Update SquareRoot.java

--- Data-Structures/src/arrays/SquareRoot.java
+++ Data-Structures/src/arrays/SquareRoot.java
@@ -1,28 +1,29 @@
 package arrays;
 
+/*
+ * Find the square root of a number.
+ * If the number is not a perfect square, then return the square root of the 
+ * closest perfect square smaller than that number. 
+ * 
+ */
 public class SquareRoot {
 	public static void main(String[] args) throws InterruptedException {
-		int x = 17;
-		int sqrt = getSquareRoot(x);
+		int x = 10;
+		int sqrt = getSquareRoot(x, 0, x);
 		System.out.println("Square root of " + x + " is: " + sqrt);
 	}
 
-	private static int getSquareRoot(int x) throws InterruptedException {
-		int start = 0;
-		int end = x;
-		int mid = 0;
-		while (start <= end) {
-			mid = (start + end) / 2;
-			if (mid * mid == x)
-				return mid;
-			if (start == mid)
-				return mid;
-			else if (mid * mid > x)
-				end = mid;
-			else
-				start = mid;
-		}
+	private static int getSquareRoot(int x, int start, int end) {
+		int mid = (start + end) / 2;
 
-		return -1;
+		if (start == mid)
+			return start;
+
+		if (mid * mid == x)
+			return mid;
+		else if (mid * mid > x)
+			return getSquareRoot(x, start, mid);
+		else
+			return getSquareRoot(x, mid, end);
 	}
 }
